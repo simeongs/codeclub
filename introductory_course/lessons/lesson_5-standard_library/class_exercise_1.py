d9c5b5cc4cdbaf59d4923161a2a64f4d21c0124f
Simeon <simeon.balson@growthstreet.co.uk>
2018-12-13 11:56:23 +0000

    class exercise 1

---
+++ introductory_course/lessons/lesson_5-standard_library/class_exercise_1.py
@@ -0,0 +1,12 @@
+
+#complete the funtion below to return the middle value 
+
+def get_middle_value(thing):
+	#finish this block of code
+	pass
+
+
+
+print(get_middle_value("#codeclub"))
+print(get_middle_value(["bakerloo", "central", "circle", "district", "hammersmith", "jubilee", "metropolitan", "nothern", "picadilly", "victoria", "waterloo and city"]))
+print(get_middle_value((1, 2, 3, 4)))
