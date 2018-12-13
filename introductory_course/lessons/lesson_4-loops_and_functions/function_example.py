2fd92af4732e07750cdf9ab22778cdc524bd20d2
Simeon <simeon.balson@growthstreet.co.uk>
2018-12-13 11:48:40 +0000

    lessons folder

---
+++ introductory_course/lessons/lesson_4-loops_and_functions/function_example.py
@@ -0,0 +1,14 @@
+ 
+def make_plural(string):
+	if string[-1] != "s":
+		string += "s"
+	else:
+		return "already plural"
+
+	return string
+
+
+list_of_words = ["cat", "dogs", "cakes", "forks", "sock" ,"shoe"]
+
+for word in list_of_words:
+	print(make_plural(word))
